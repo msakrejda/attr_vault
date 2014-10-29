bf2fd15bf06d33f2d610e717aa600e6166d829a5
Maciek Sakrejda <maciek@heroku.com>
2014-10-29 11:04:19 -0700

    Additional specs

--- spec/attr_vault_spec.rb
+++ spec/attr_vault_spec.rb
@@ -92,7 +92,51 @@ describe AttrVault do
         expect(s.secret).to be_nil
         expect(s.secret_encrypted).to be_nil
       end
+    end
+  end
 
+  context "renaming database fields" do
+    let(:key_data) {
+      [{
+        id: '80a8571b-dc8a-44da-9b89-caee87e41ce2',
+        value: 'aFJDXs+798G7wgS/nap21LXIpm/Rrr39jIVo2m/cdj8=',
+        created_at: Time.now }].to_json
+    }
+
+    it "supports renaming the encrypted and hmac fields" do
+      k = key_data
+      item = Class.new(Sequel::Model(:items)) do
+        include AttrVault
+        vault_keyring k
+        vault_attr :classified_info,
+          encrypted_field: :secret_encrypted,
+          hmac_field: :secret_hmac
+      end
+
+      secret = "we've secretly replaced the fine coffee they usually serve with Folgers Crystals"
+      s = item.create(classified_info: secret)
+      s.reload
+      expect(s.classified_info).to eq secret
+      expect(s.secret_encrypted).not_to eq secret
+      expect(s.secret_hmac).not_to be_nil
+    end
+
+    it "supports renaming the key id field" do
+      k = key_data
+      item = Class.new(Sequel::Model(:items)) do
+        include AttrVault
+        vault_keyring k, key_field: :alt_key_id
+        vault_attr :secret
+      end
+
+      secret = "up up down down left right left right b a"
+      s = item.create(secret: secret)
+      s.reload
+      expect(s.secret).to eq secret
+      expect(s.secret_encrypted).not_to eq secret
+      expect(s.secret_hmac).not_to be_nil
+      expect(s.alt_key_id).not_to be_nil
+      expect(s.key_id).to be_nil
     end
   end
 end
