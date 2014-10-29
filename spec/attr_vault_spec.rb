2e059ba502710f26dd4fbc67304289f896ce5ef4
Maciek Sakrejda <maciek@heroku.com>
2014-10-29 12:04:07 -0700

    More specs

--- spec/attr_vault_spec.rb
+++ spec/attr_vault_spec.rb
@@ -3,9 +3,10 @@ require 'json'
 
 describe AttrVault do
   context "with a single encrypted column" do
+    let(:key_id)   { '80a8571b-dc8a-44da-9b89-caee87e41ce2' }
     let(:key_data) {
       [{
-        id: '80a8571b-dc8a-44da-9b89-caee87e41ce2',
+        id: key_id,
         value: 'aFJDXs+798G7wgS/nap21LXIpm/Rrr39jIVo2m/cdj8=',
         created_at: Time.now }].to_json
     }
@@ -54,6 +55,13 @@ describe AttrVault do
         expect(s.secret).to be_nil
         expect(s.secret_encrypted).to be_nil
       end
+
+      it "stores the key id" do
+        secret = 'it was professor plum with the wrench in the library'
+        s = item.create(secret: secret)
+        s.reload
+        expect(s.key_id).to eq(key_id)
+      end
     end
 
     context "with an existing object" do
@@ -92,6 +100,14 @@ describe AttrVault do
         expect(s.secret).to be_nil
         expect(s.secret_encrypted).to be_nil
       end
+
+      it "stores the key id" do
+        secret = 'animal style'
+        s = item.create
+        s.update(secret: secret)
+        s.reload
+        expect(s.key_id).to eq(key_id)
+      end
     end
   end
 
@@ -122,6 +138,92 @@ describe AttrVault do
       s.reload
       expect(s.secret).to eq secret1
       expect(s.other).to eq secret2
+    end
+  end
+
+  context "with items encrypted with an older key" do
+    let(:key1_id)  { '80a8571b-dc8a-44da-9b89-caee87e41ce2' }
+    let(:key1)     {
+      {
+       id: key1_id,
+       value: 'aFJDXs+798G7wgS/nap21LXIpm/Rrr39jIVo2m/cdj8=',
+       created_at: Time.new(2014, 1, 1, 0, 0, 0)
+      }
+    }
+
+    let(:key2_id)  { '0a85781b-d8ac-4a4d-89b9-acee874e1ec2' }
+    let(:key2)     {
+      {
+       id: key2_id,
+       value: 'hUL1orBBRckZOuSuptRXYMV9lx5Qp54zwFUVwpwTpdk=',
+       created_at: Time.new(2014, 2, 1, 0, 0, 0)
+      }
+    }
+    let(:partial_keyring) {
+      [key1].to_json
+    }
+
+    let(:full_keyring) {
+      [key1, key2].to_json
+    }
+    let(:item1) {
+      k = partial_keyring
+      Class.new(Sequel::Model(:items)) do
+        include AttrVault
+        vault_keyring k
+        vault_attr :secret
+        vault_attr :other
+      end
+    }
+    let(:item2) {
+      k = full_keyring
+      Class.new(Sequel::Model(:items)) do
+        include AttrVault
+        vault_keyring k
+        vault_attr :secret
+        vault_attr :other
+      end
+    }
+
+    it "rewrites the items using the current key" do
+      secret1 = 'mrs. doubtfire is really a man'
+      secret2 = 'tootsie? also a man'
+      record = item1.create(secret: secret1)
+      expect(record.key_id).to eq key1_id
+      expect(record.secret).to eq secret1
+
+      old_secret_encrypted = record.secret_encrypted
+      old_secret_hmac = record.secret_hmac
+
+      new_key_record = item2[record.id]
+      new_key_record.update(secret: secret2)
+      new_key_record.reload
+
+      expect(new_key_record.key_id).to eq key2_id
+      expect(new_key_record.secret).to eq secret2
+      expect(new_key_record.secret_encrypted).not_to eq old_secret_encrypted
+      expect(new_key_record.secret_hmac).not_to eq old_secret_hmac
+    end
+
+    it "rewrites the items using the current key even if they are not updated" do
+      secret1 = 'the planet of the apes is really earth'
+      secret2 = 'the answer is 42'
+      record = item1.create(secret: secret1)
+      expect(record.key_id).to eq key1_id
+      expect(record.secret).to eq secret1
+
+      old_secret_encrypted = record.secret_encrypted
+      old_secret_hmac = record.secret_hmac
+
+      new_key_record = item2[record.id]
+      new_key_record.update(other: secret2)
+      new_key_record.reload
+
+      expect(new_key_record.key_id).to eq key2_id
+      expect(new_key_record.secret).to eq secret1
+      expect(new_key_record.secret_encrypted).not_to eq old_secret_encrypted
+      expect(new_key_record.secret_hmac).not_to eq old_secret_hmac
+      expect(new_key_record.other).to eq secret2
     end
   end
 
